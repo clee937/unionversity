04a80da05fedc197a89225e32e6b8c95704552a1
Cheryl Lee <clee0937@gmail.com>
2025-04-07 15:59:46 +0100

    added types for Course, StudyGroup, and SearchEventsOptions

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import courses from "./courses";
+import studyGroups from "./studyGroups";
+
+type Course = {
+  id: number;
+  studyGroupId: number;
+  title: string;
+  keywords: string[];
+  eventType: string;
+};
+
+type StudyGroup = {
+  id: number;
+  courseId: number;
+  title: string;
+  keywords: string[];
+  eventType: string;
+};
+
+type SearchEventsOptions = {
+  query: string | number;
+  eventType: "courses" | "groups";
+};
